e3aa7302f44a327a95663713e0b12842762630fa
Your Name <asadiqbal7@icloud.com>
2024-08-27 00:51:17 +0500

    vector embedding changed huggingface2

--- vectordbsearch_tools.py
+++ vectordbsearch_tools.py
@@ -2,7 +2,7 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores.faiss import FAISS
 from langchain_community.document_loaders import DirectoryLoader
-from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+
 
 from dotenv import load_dotenv
 import os
